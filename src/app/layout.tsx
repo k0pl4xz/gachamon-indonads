aa06f00c985385552cdb4ffea85ab37096ea25fa
Ari Nugraha <k0pl4xz@users.noreply.github.com>
2025-06-13 14:28:04 +0700

    Update layout.tsx
    change : tittle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Gacha Mon - Undian Mon",
+  title: "GachaMon - Indonads",
   description: "For Indonads User",
 };
 
